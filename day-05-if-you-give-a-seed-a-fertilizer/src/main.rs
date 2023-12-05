471007896bc9cefac13fb9f7c8db4bac3db79483
hassomepicklejuice <hassomepicklejuice@proton.me>
2023-12-05 17:13:11 +0100

    day05: optimization succeeded!

--- day-05-if-you-give-a-seed-a-fertilizer/src/main.rs
+++ day-05-if-you-give-a-seed-a-fertilizer/src/main.rs
@@ -1,78 +1,154 @@
-use std::{collections::HashMap, io, str::FromStr};
+use std::{cmp::Ordering, collections::HashMap, io, ops, str::FromStr};
 
 fn main() {
     let input = io::read_to_string(io::stdin()).unwrap();
     let output = part1(&input);
     println!("part1: {output}");
-    // let output = part2(&input);
-    // println!("part2: {output}");
+    let output = part2(&input);
+    println!("part2: {output}");
 }
 
-#[derive(Debug, Default, PartialEq, Eq, PartialOrd, Ord)]
+type Range = ops::Range<usize>;
+
+#[derive(Debug, Default, PartialEq, Eq)]
 struct RangeMap {
-    source_start: usize,
-    range_len: usize,
-    dest_start: usize,
+    src: Range,
+    dest: Range,
+}
+
+impl PartialOrd for RangeMap {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for RangeMap {
+    fn cmp(&self, other: &Self) -> Ordering {
+        cmp_ranges(&self.src, &other.src)
+    }
+}
+
+fn cmp_ranges(lhs: &Range, rhs: &Range) -> Ordering {
+    match lhs.start.cmp(&rhs.start) {
+        Ordering::Less => Ordering::Less,
+        Ordering::Greater => Ordering::Greater,
+        Ordering::Equal => lhs.end.cmp(&rhs.end),
+    }
 }
 
 impl RangeMap {
-    fn map(&self, source: usize) -> Option<usize> {
-        let diff = source.wrapping_sub(self.source_start);
-        if diff < self.range_len {
-            Some(self.dest_start + diff)
-        } else {
-            None
+    fn map_val(&self, val: usize) -> Option<usize> {
+        match self.src.contains(&val) {
+            true => Some(val - self.src.start + self.dest.start),
+            false => None,
         }
     }
+    fn map_range(&self, range: &Range) -> (u8, [Range; 3]) {
+        let mut tag = 0;
+        let res = [
+            // Range before: kept the same
+            match range.start < self.src.start {
+                true => {
+                    tag |= 0b100;
+                    range.start..range.end.min(self.src.start)
+                }
+                false => Default::default(),
+            },
+            // Range between: mapped
+            match range.start < self.src.end && range.end > self.src.start {
+                true => {
+                    tag |= 0b010;
+                    let start = range.start.max(self.src.start);
+                    let end = range.end.min(self.src.end);
+                    self.map_val(start).unwrap()..(self.map_val(end - 1).unwrap() + 1)
+                }
+                false => Default::default(),
+            },
+            // Range after: kept the same
+            match range.end >= self.src.end {
+                true => {
+                    tag |= 0b001;
+                    range.start.max(self.src.end)..range.end
+                }
+                false => Default::default(),
+            },
+        ];
+
+        (tag, res)
+    }
+
     fn from_line(input: &str) -> Self {
         let mut iter = input.split_whitespace();
+
+        let dest_start = iter.next().unwrap().parse().unwrap();
+        let source_start = iter.next().unwrap().parse().unwrap();
+        let range_len: usize = iter.next().unwrap().parse().unwrap();
+
         Self {
-            dest_start: iter.next().unwrap().parse().unwrap(),
-            source_start: iter.next().unwrap().parse().unwrap(),
-            range_len: iter.next().unwrap().parse().unwrap(),
+            src: source_start..(source_start + range_len),
+            dest: dest_start..(dest_start + range_len),
         }
     }
 }
 
 #[derive(Debug)]
-struct Map {
+struct FullMap {
     ranges: Vec<RangeMap>,
 }
 
-impl Map {
-    fn sort_unstable(&mut self) {
-        self.ranges.sort_unstable();
+impl From<Vec<RangeMap>> for FullMap {
+    fn from(mut input: Vec<RangeMap>) -> Self {
+        input.sort_unstable();
+        Self { ranges: input }
     }
-    fn map_val(&self, source: usize) -> usize {
+}
+
+impl FullMap {
+    fn map_val(&self, val: usize) -> usize {
         self.ranges
             .iter()
-            .flat_map(|r| r.map(source))
+            .flat_map(|range| range.map_val(val))
             .next()
-            .unwrap_or(source)
+            .unwrap_or(val)
     }
-    /// Assumes sorted ranges.
-    fn map_ranges(&self, other: &Self) -> Self {
-        let mut ranges = Vec::with_capacity(other.ranges.len());
-        for range in other.ranges.iter() {
-            dbg!(&range);
-            let res = self.ranges.binary_search(&range).unwrap_or_else(|n| n - 1);
-            let lower = &self.ranges[res];
-            dbg!(lower);
+    fn map_range(&self, mut range: Range) -> Vec<Range> {
+        let mut has_after = false;
+        let mut res = vec![];
+        for r in self.ranges.iter() {
+            let (tag, [before, during, after]) = r.map_range(&range);
+            if tag & 0b100 > 0 {
+                res.push(before);
+            }
+            if tag & 0b010 > 0 {
+                res.push(during);
+            }
+            if tag & 0b001 > 0 {
+                has_after = true;
+                range = after;
+            } else {
+                has_after = false;
+                break;
+            }
         }
-        Self { ranges }
+        if has_after {
+            res.push(range);
+        }
+        res
     }
     fn from_block(input: &str) -> (&str, &str, Self) {
         let mut lines = input.lines();
         let (kind, _) = lines.next().unwrap().split_once(' ').unwrap();
         let (source, dest) = kind.split_once("-to-").unwrap();
-        let mut map = Self {
-            ranges: lines.map(RangeMap::from_line).collect(),
-        };
-        map.sort_unstable();
+        let mut map = Self::from(lines.map(RangeMap::from_line).collect::<Vec<RangeMap>>());
         (source, dest, map)
     }
-    fn min(&self) -> usize {
-        todo!()
+    fn map_ranges(&self, ranges: &[Range]) -> Vec<Range> {
+        let mut res = Vec::with_capacity(ranges.len());
+        for range in ranges {
+            res.append(&mut self.map_range(range.clone()));
+        }
+        res.sort_unstable_by(cmp_ranges);
+        res
     }
 }
 
@@ -87,8 +163,8 @@ fn part1(input: &str) -> usize {
         .split_whitespace()
         .flat_map(FromStr::from_str);
 
-    let maps: HashMap<&str, (&str, Map)> = blocks
-        .map(Map::from_block)
+    let maps: HashMap<&str, (&str, FullMap)> = blocks
+        .map(FullMap::from_block)
         .map(|(src, dst, map)| (src, (dst, map)))
         .collect();
 
@@ -117,26 +193,21 @@ fn part2(input: &str) -> usize {
         .flat_map(FromStr::from_str)
         .collect::<Vec<usize>>();
 
-    let seeds: Vec<RangeMap> = seeds
-        .chunks(2)
-        .map(|r| RangeMap {
-            dest_start: r[0],
-            source_start: r[0],
-            range_len: r[1],
-        })
-        .collect();
-    let mut seeds: Map = Map { ranges: seeds };
+    let mut seeds: Vec<Range> = seeds.chunks(2).map(|r| r[0]..(r[0] + r[1])).collect();
 
     // Very slow -> create a single map from seed to location
     // or reverse mapping?
     // or boundary mapping/analysis
-    let maps: Vec<Map> = blocks.map(Map::from_block).map(|(_, _, map)| map).collect();
+    let maps: Vec<FullMap> = blocks
+        .map(FullMap::from_block)
+        .map(|(_, _, map)| map)
+        .collect();
 
     for map in maps {
         seeds = map.map_ranges(&seeds);
     }
 
-    seeds.min()
+    seeds.first().unwrap().start
 }
 
 fn _part2(input: &str) -> usize {
@@ -156,7 +227,10 @@ fn _part2(input: &str) -> usize {
     // Very slow -> create a single map from seed to location
     // or reverse mapping?
     // or boundary mapping/analysis
-    let maps: Vec<Map> = blocks.map(Map::from_block).map(|(_, _, map)| map).collect();
+    let maps: Vec<FullMap> = blocks
+        .map(FullMap::from_block)
+        .map(|(_, _, map)| map)
+        .collect();
 
     seeds
         .map(|mut s| {
