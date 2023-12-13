0e2aa36660f0b7098454c6c4d93362981ecb8f65
hassomepicklejuice <hassomepicklejuice@proton.me>
2023-12-13 15:56:26 +0100

    day13: falling behind

--- day-13-point-of-incidence/src/main.rs
+++ day-13-point-of-incidence/src/main.rs
@@ -1,3 +1,33 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn part1(input: &str) -> usize {
+    let patterns = input
+        .split("\n\n")
+        .map(|pat| pat.lines().map(str::as_bytes).collect::<Vec<_>>())
+        .collect::<Vec<_>>();
+    todo!()
+}
+
+const EXAMPLE: &str = "#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#";
+
+#[test]
+fn test1() {
+    let output = part1(EXAMPLE);
+    assert_eq!(output, 405);
+}
